d1f53283b900b6dee5f4f636446fea9d143800fc
jeffmhopkins <jeff.m.hopkins@gmail.com>
2024-05-07 21:23:14 -0400

    Delete src directory

--- src/prune-recordings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    find /var/www/html/recordings/ -type f -name '*No user*' -delete
-    find /var/www/html/recordings/ -type f -name '*No user*' -size 1000k -delete
-    sleep 1m
-done
